c414a39c9816d69c62c7c9dfaf9d3b87e2e79331
GTsirekidze <g_tsirekidze@cu.edu.ge>
2022-05-23 23:24:45 +0400

    ""

--- cache.go
+++ cache.go
@@ -3,21 +3,41 @@ package cache
 import "time"
 
 type Cache struct {
+	value       string
+	isPermanent bool
 }
 
-func NewCache() Cache {
-	return Cache{}
+type Caches struct {
+	mp map[string]Cache
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func NewCache(value string, isPermanent bool) Cache {
+	return Cache{value, isPermanent}
 }
 
-func (receiver) Put(key, value string) {
+func (caches Caches) Get(key string) (string, bool) {
+	return caches.mp[key].value, true
 }
 
-func (receiver) Keys() []string {
+func (caches *Caches) Put(key, value string) {
+	caches.mp[key] = Cache{value, true}
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (caches Caches) Keys() []string {
+	var ans []string
+	for k, _ := range caches.mp {
+		ans = append(ans, k)
+	}
+
+	return ans
+}
+
+func (caches *Caches) PutTill(key, value string, deadline time.Time) {
+	caches.mp[key] = Cache{value, true}
+
+	start := time.Now().UnixNano() / int64(time.Millisecond)
+	end := deadline.UnixNano() / int64(time.Millisecond)
+	time.Sleep(time.Duration(end - start))
+
+	delete(caches.mp, key)
 }
