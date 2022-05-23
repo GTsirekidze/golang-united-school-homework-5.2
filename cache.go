0ce3eccdde83bb5d6006f0d49234779733c04d70
GTsirekidze <g_tsirekidze@cu.edu.ge>
2022-05-23 23:55:03 +0400

    ""

--- cache.go
+++ cache.go
@@ -3,16 +3,15 @@ package cache
 import "time"
 
 type Cache struct {
-	value       string
-	isPermanent bool
+	value string
 }
 
 type Caches struct {
 	mp map[string]Cache
 }
 
-func NewCache(value string, isPermanent bool) Cache {
-	return Cache{value, isPermanent}
+func NewCache(value string) Cache {
+	return Cache{value}
 }
 
 func (caches Caches) Get(key string) (string, bool) {
@@ -20,7 +19,7 @@ func (caches Caches) Get(key string) (string, bool) {
 }
 
 func (caches *Caches) Put(key, value string) {
-	caches.mp[key] = Cache{value, true}
+	caches.mp[key] = Cache{value}
 }
 
 func (caches Caches) Keys() []string {
@@ -33,7 +32,7 @@ func (caches Caches) Keys() []string {
 }
 
 func (caches *Caches) PutTill(key, value string, deadline time.Time) {
-	caches.mp[key] = Cache{value, true}
+	caches.mp[key] = Cache{value}
 
 	start := time.Now().UnixNano() / int64(time.Millisecond)
 	end := deadline.UnixNano() / int64(time.Millisecond)
